8249319076aaab76307a27ac583a541736091128
Reza Ghifary <fachrezaghifary@gmail.com>
2021-01-27 12:45:15 +0700

    add forgot password logic

---
+++ src/pages/forgot-password.tsx
@@ -0,0 +1,54 @@
+import { Box, Button } from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
+import { withUrqlClient } from 'next-urql';
+// import router from 'next/dist/next-server/lib/router/router';
+import React, { useState } from 'react';
+import InputField from '../components/InputField';
+import Wrapper from '../components/Wrapper';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { useForgotPasswordMutation } from '../generated/graphql';
+
+const ForgotPassword: React.FC<{}> = ({}) => {
+    const [complete, setComplete] = useState(false);
+    const [, forgotPassword] = useForgotPasswordMutation();
+    return (
+        <Wrapper variant="small">
+            <Formik
+                initialValues={{ email: '' }}
+                onSubmit={async (values) => {
+                    await forgotPassword(values);
+                    setComplete(true);
+                }}
+            >
+                {({ isSubmitting }) =>
+                    complete ? (
+                        <Box>
+                            If your account exist, we already sent you an email
+                        </Box>
+                    ) : (
+                        <Form>
+                            <Box mt={5}>
+                                <InputField
+                                    name="email"
+                                    placeholder="email"
+                                    label="Email"
+                                    type="email"
+                                />
+                            </Box>
+                            <Button
+                                isLoading={isSubmitting}
+                                mt={4}
+                                type="submit"
+                                colorScheme="teal"
+                            >
+                                Submit
+                            </Button>
+                        </Form>
+                    )
+                }
+            </Formik>
+        </Wrapper>
+    );
+};
+
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Form, Formik } from 'formik';
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
 // import { gql, useMutation } from 'urql';
 import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from 'next/router';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
+import NextLink from 'next/link';
 
 interface registerProps {}
 
@@ -50,6 +51,17 @@ const Login: React.FC<{}> = ({}) => {
                                 type="password"
                             />
                         </Box>
+                        <Flex>
+                            <NextLink href="/forgot-password">
+                                <Link
+                                    mt={2}
+                                    ml="auto"
+                                    style={{ color: 'blue' }}
+                                >
+                                    forgot password?
+                                </Link>
+                            </NextLink>
+                        </Flex>
                         <Button
                             isLoading={isSubmitting}
                             mt={5}
